e6ee5b95b70b49b31881469d47cc35563ae26d61
quiet-visage <0xffigor@gmail.com>
2025-05-14 12:44:52 -0300

    prototype

---
+++ ticketFilters.tsx
@@ -0,0 +1,53 @@
+import { Input } from "@heroui/input";
+import { DateValue } from "@internationalized/date";
+import { Search } from "lucide-react";
+import { DateRangePick } from "./dateRangePick";
+import { PriorityFilter } from "./priorityFilter";
+import { StatusFilter } from "./statusFilter";
+
+interface TicketFiltersSelectProps {
+  minDate: DateValue;
+  maxDate: DateValue;
+  search: string;
+  priority: Set<string>;
+  status: Set<string>;
+  setMinDate: (v: DateValue) => void;
+  setMaxDate: (v: DateValue) => void;
+  setSearch: (v: string) => void;
+  setPriority: (v: Set<string>) => void;
+  setStatus: (v: Set<string>) => void;
+  className?: string;
+}
+
+export const TicketFiltersSelect = ({
+  minDate,
+  maxDate,
+  search,
+  priority,
+  status,
+  setMinDate,
+  setMaxDate,
+  setSearch,
+  setPriority,
+  setStatus,
+  className = "",
+}: TicketFiltersSelectProps) => {
+  return (
+    <div className="flex gap-2 items-center w-full">
+      <Input
+        value={search}
+        onValueChange={setSearch}
+        startContent={<Search size={16} />}
+        placeholder="procurar"
+      />
+      <DateRangePick
+        minDate={minDate}
+        maxDate={maxDate}
+        setMinDate={setMinDate}
+        setMaxDate={setMaxDate}
+      />
+      <PriorityFilter filters={priority} setFilters={setPriority} />
+      <StatusFilter filters={status} setFilters={setStatus} />
+    </div>
+  );
+};
