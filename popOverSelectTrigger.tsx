6b0046f8797628415e29148abd00c51ee7caa1fa
quiet-visage <0xffigor@gmail.com>
2025-05-19 10:57:08 -0300

    prototype

--- popOverSelectTrigger.tsx
+++ popOverSelectTrigger.tsx
@@ -4,23 +4,29 @@ import { AnimatedChevron } from "./animatedChevron";
 
 export const PopoverSelectTrigger = ({
   className = "",
+  hideChevron = false,
   children,
   ...props
 }: {
   className?: string;
+  hideChevron?: boolean;
   children: ReactNode;
 }) => {
   const [open, setOpen] = useState<boolean>(false);
-
+  const endContent = hideChevron ? (
+    <></>
+  ) : (
+    <AnimatedChevron open={open} className="absolute end-3" />
+  );
   return (
     <Button
       {...props}
       onPress={() => setOpen(!open)}
-      endContent={<AnimatedChevron open={open} className="absolute end-3" />}
+      endContent={endContent}
       variant="flat"
       className={
-        className +
-        " inline-flex flex-row items-center w-full bg-default-100 justify-start"
+        "inline-flex flex-row items-center w-full bg-default-100 justify-start " +
+        className
       }
     >
       {children}
