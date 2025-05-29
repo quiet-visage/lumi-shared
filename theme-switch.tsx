3271c68664bca708c0bbf0bdc815b7ccb591ce9e
igor <0xffigor@gmail.com>
2025-05-28 22:18:44 -0300

    prototype

--- theme-switch.tsx
+++ theme-switch.tsx
@@ -73,7 +73,7 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
         {!isSelected || isSSR ? (
           <SunFilledIcon size={22} />
         ) : (
-          <MoonFilledIcon size={22} />
+          <MoonFilledIcon size={22} color="#52525b" />
         )}
       </div>
     </Component>
