8c06be1bd604f94203a70de96e4dd4d9671a97ff
Igor Martins <igor.martins@setelagos.local>
2025-07-28 12:47:03 -0300

    prototype

---
+++ hooks/useFileUpload.ts
@@ -0,0 +1,84 @@
+import { TicketUser } from "@/app/models";
+import { AdminTokenContext } from "@/app/providers";
+import { FileUploadStatus } from "@/components/admin/fileUploadView";
+import { api } from "@/config/api";
+import { addToast } from "@heroui/react";
+import { useContext, useState } from "react";
+import { decodeToken } from "react-jwt";
+
+export const useFileUpload = () => {
+  const token = useContext(AdminTokenContext);
+  const decodedToken: { [key: string]: string } | null = decodeToken(token);
+  if (decodedToken === null) throw "invalid token";
+  const user: TicketUser = {
+    _id: decodedToken._id,
+    name: decodedToken.name,
+    sector: decodedToken.sector,
+    branch: decodedToken.branch,
+    viewScope: Number(decodedToken.viewScope),
+    disabled: decodedToken.disabled === "true" ? true : false,
+  };
+
+  const [filesToUpload, setFilesToUpload] = useState<File[]>([]);
+  const [fileUploadStatuses, setFileUploadStatuses] = useState<
+    FileUploadStatus[]
+  >([]);
+  const [isUploading, setIsUploading] = useState(false);
+
+  const beginUpload = async () => {
+    setIsUploading(true);
+    const uploadPromises = filesToUpload.map(async (file, index) => {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append(
+        "body",
+        JSON.stringify({
+          fileName: file.name,
+          uploadedBy: user,
+          createdAt: new Date(),
+        })
+      );
+
+      setFileUploadStatuses(
+        filesToUpload.map((v) => ({ fileName: v.name, progress: 0 }))
+      );
+      try {
+        await api.post("/upload_file", formData, {
+          headers: { Authorization: token },
+          onUploadProgress: (progress) => {
+            if (progress.progress === undefined) return;
+            setFileUploadStatuses((currentStatuses) =>
+              currentStatuses.map((status, i) =>
+                i === index
+                  ? { ...status, progress: progress.progress! }
+                  : status
+              )
+            );
+          },
+        });
+        return { file: file.name, uploaded: true };
+      } catch (e) {
+        console.error(`Failed to upload ${file.name}`, e);
+        setFileUploadStatuses((currentStatuses) =>
+          currentStatuses.map((status, i) =>
+            i === index ? { ...status, progress: -1 } : status
+          )
+        );
+        addToast({ title: "Upload failed  " + file.name });
+        return { file: file.name, uploaded: false };
+      }
+    });
+    const results = await Promise.allSettled(uploadPromises);
+    setIsUploading(false);
+    return results;
+  };
+
+  return {
+    filesToUpload,
+    setFilesToUpload,
+    fileUploadStatuses,
+    setFileUploadStatuses,
+    beginUpload,
+    isUploading,
+  };
+};
