0ee4b0f648b0cf5a33840a474bbcf1df0f11193c
igor <0xffigor@gmail.com>
2025-09-20 21:52:12 -0300

    prototype

--- ticket/ticketFileLink.tsx
+++ ticket/ticketFileLink.tsx
@@ -2,6 +2,6 @@ import { FileLink } from "../fileLink";
 
 export const TicketFileLink = ({ filename }: { filename: string }) => {
   return (
-    <FileLink url={`http://${process.env.API_HOST}:8000/file/${filename}`} />
+    <FileLink url={`${process.env.BASE_URL}file/${filename}`} />
   );
 };
